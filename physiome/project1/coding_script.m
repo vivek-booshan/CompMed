117d6801cfe01975d814b1fd1bd2fa6817b40dfd
Vivek Booshan <vivek.booshan@gmail.com>
2023-11-07 15:23:59 -0500

    add q3 script

--- physiome/project1/coding_script.m
+++ physiome/project1/coding_script.m
@@ -9,7 +9,7 @@ base_path = pwd;
 s00020_struct = dir(fullfile(".", "**/s00020", "*.txt"));
 s00020_folder = s00020_struct.folder;
 s00020_folder = convertCharsToStrings(s00020_folder);
-[ABP_125Hz_FILE, S00020_FILE] =s00020_struct.name;
+[ABP_125Hz_FILE, S00020_FILE] = s00020_struct.name;
 
 % Recursively search to find the 2analyze folder and set path
 analyze2_path = dir(fullfile(".", "**/2analyze", "*.m")).folder;
@@ -44,7 +44,7 @@ cd(base_path)
 
 time10hr_pulse20 = time(idx10 : idx10 + 20*125);
 abp10hr_pulse20 = abp(idx10 : idx10 + 20*125);
-[locs, idxs] = findpeaks(-abp10hr_pulse20);
+[~, idxs] = findpeaks(-abp10hr_pulse20);
 sample_num = 1:length(time10hr_pulse20);
 
 
@@ -52,18 +52,22 @@ subplot(2, 1, 1);
 hold on;
 plot(sample_num, abp10hr_pulse20);
 plot(sample_num(idxs(2:2:end)), abp10hr_pulse20(idxs(2:2:end)), 'k*');
-plot(sample_num(idxs(1:2:end)), abp_20pulse(idxs(1:2:end)), 'ro');
+plot(sample_num(idxs(1:2:end)), abp10hr_pulse20(idxs(1:2:end)), 'ro');
 title("10 Hr")
 axis padded;
+xlim('tight');
 
 time11hr_pulse20 = time(idx11 : idx11 + 20*125);
 abp11hr_pulse20 = abp(idx11 : idx11 + 20*125);
 [locs, idxs] = findpeaks(-abp11hr_pulse20);
 sample_num = 1:length(time11hr_pulse20);
+
+
 subplot(2, 1, 2);
 hold on;
 plot(sample_num, abp11hr_pulse20);
-plot(sample_num(idxs(2:2:end)), abp10hr_pulse20(idxs(2:2:end)), 'k*');
-plot(sample_num(idxs(1:2:end)), abp_20pulse(idxs(1:2:end)), 'ro')
+plot(sample_num(idxs(2:2:end)), abp11hr_pulse20(idxs(2:2:end)), 'ro');
+plot(sample_num(idxs(1:2:end)), abp11hr_pulse20(idxs(1:2:end)), 'k*')
 title("11 Hr")
 axis padded;
+xlim('tight');
---
+++ physiome/project1/q3_script.m
@@ -0,0 +1,19 @@
+base_path = pwd;
+
+s00020_struct = dir(fullfile(".", "**/s00020", "*.txt"));
+s00020_folder = s00020_struct.folder;
+s00020_folder = convertCharsToStrings(s00020_folder);
+[ABP_125Hz_FILE, S00020_FILE] = s00020_struct.name;
+
+% Recursively search to find the 2analyze folder and set path
+analyze2_path = dir(fullfile(".", "**/3estimate", "*.m")).folder;
+
+% load ABP data (125 Hz sampled) using ascii flag
+s00020_DATA = load(s00020_folder + "/" + s00020_FILE, "-ascii");
+opts = detectImportOptions(base_path + "/s00020/" + s00020_FILE);
+opts.SelectedVariableNames = ["ElapsedTime", "HR", "ABPSys", "ABPDias"];
+T = readtable(s00020_DATA, opts);
+r = find(T.CO, ~0, 'first');
+idx_12hr = find(T.ElapsedTime == 43200);
+
+x = T.HR .* ((T.ABPSys - T.ABPDias) ./ (T.ABPSys + T.ABPDias));
