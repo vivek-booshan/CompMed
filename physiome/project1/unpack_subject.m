3fded19623ec10267f06b36ddf1a69be68b2d8cf
Vivek Booshan <vivek.booshan@gmail.com>
2023-11-11 16:22:04 -0500

    rm separate subject init files

--- physiome/project1/unpack_subject.m
+++
@@ -1,7 +0,0 @@
-function subject = unpack_subject(name)
-    [subject_abp_file, subject_file] = get_subject_files(name);
-    subject_abp_data = load(subject_abp_file, "-ascii");
-    subject.time = subject_abp_data(:, 1);
-    subject.abp = subject_abp_data(:, 2);
-    subject.table = readtable(subject_file);
-end
