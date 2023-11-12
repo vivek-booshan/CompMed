2aeafef994c0beb063bc1381de2b79b7e0e672dd
Vivek Booshan <vivek.booshan@gmail.com>
2023-11-12 15:28:35 -0500

    work on q2 and q4. Still need to fix q2 and some q4. Also adds additional features to subject class

--- physiome/project1/Subject.m
+++ physiome/project1/Subject.m
@@ -54,7 +54,7 @@ classdef Subject
             % end
             filter = (obj.features(:, 1) >= idx) & (obj.features(:, 1) <= idx + pulses*125);
             onset_idxs = obj.features(filter, 3);
-            onset_idxs = onset_idxs - 125*time_pulses(1);
+            onset_idxs = onset_idxs - idx;
             onset_idxs = onset_idxs(onset_idxs > 0);
 
             offset_minslope_idxs = obj.features(filter, 11);
@@ -64,6 +64,19 @@ classdef Subject
             offset_beatperiod_idxs = obj.features(filter, 9);
             offset_beatperiod_idxs = offset_beatperiod_idxs - 125*time_pulses(1);
             offset_beatperiod_idxs = offset_beatperiod_idxs(offset_beatperiod_idxs > 0); 
+        end
+        function [co, to, told, fea] = estimateCO(obj, estID, filt_order)
+            base_path = pwd;
+            path_3estimate = dir(fullfile(".", "**/3estimate", "*.m")).folder;
+            cd(path_3estimate);
+            [co, to, told, fea] = estimateCO_v3(obj.onset_times, obj.features, obj.beatQ, estID, filt_order);
+            cd(base_path);
+        end
+        function [k_CO, k_PP, k_MAP, k_HR] = get_k(obj, CO_idxs, CO, FEA, T)
+            k_CO = CO(CO_idxs(1)) / T.CO(CO_idxs(1));
+            k_PP = FEA(CO_idxs(1), 5) / (T.ABPSys(CO_idxs(1)) - T.ABPDias(CO_idxs(1)));
+            k_MAP = FEA(CO_idxs(1), 6) / (T.ABPMean(CO_idxs(1)));
+            k_HR = FEA(CO_idxs(1), 7) / T.HR(CO_idxs(1));
         end
     end
 end
--- physiome/project1/coding_script.m
+++ physiome/project1/coding_script.m
@@ -5,7 +5,7 @@ clf; clc; clear;
 % Set present working directory as var
 base_path = pwd;
 
-% Recursively search to find subject 20 data files to set path and file
+% Recursively search to find subject data files to set path and file
 % names
 s00020 = Subject("s00020");
 s00138 = Subject("s00138");
@@ -88,7 +88,7 @@ subplot(2, 1, 2); hold on;
     offset_minslope_idxs, ...
     offset_beatperiod_idxs, ...
     abp_pulses...
-] = get_trace(s00214.time, s00214.abp, s00214.features, 11, 20)
+] = get_trace(s00214.time, s00214.abp, s00214.features, 11, 20);
 
 plot_trace(...
     sample_idxs, ...
@@ -103,34 +103,37 @@ ylim padded, xlim tight;
 
 % Get CO, onset times and features from CO estimator using Liljestrand,
 % onset times, and ABP features.
-[CO, ~, ~, FEA] = estimateCO_v3(s00020.onset_times, s00020.ABP_features, s00020.beatQ, 5, 0);
-
+[CO, ~, ~, FEA] = s00020.estimateCO(5, 0);
+[CO1, ~, ~, FEA1] = s00020.estimateCO(1, 0);
+[CO2, ~, ~, FEA2] = s00020.estimateCO(2, 0);
 % detect import options for patient 20 and select desired parameters
-opts = detectImportOptions(s00020_FILE);
-opts.SelectedVariableNames = [
-    "ElapsedTime", ...
-    "HR", ...
-    "ABPSys", ... 
-    "ABPDias", ...
-    "ABPMean", ...
-    "CO"...
-    ];
+% opts = detectImportOptions(s00020_FILE);
+% opts.SelectedVariableNames = [
+%     "ElapsedTime", ...
+%     "HR", ...
+%     "ABPSys", ... 
+%     "ABPDias", ...
+%     "ABPMean", ...
+%     "CO"...
+%     ];
 % read data as table and filter first 12 hours
 T = s00020.table(s00020.table.ElapsedTime <= 12*36e2, :);
 % get measured CO indexes
 CO_idxs = find(T.CO ~= 0);
 
 %generate time range in seconds and hrs for plotting
-time_range = 1:43200;
+time_range = 1:(12*36e2);
 timehr_range = time_range/3600;
 
 % k_A = estimate of A / true A
 % Cardiac Output (CO) ; Pulse Pressure (PP); Mean Arterial Pressure (MAP) ;
 % Heart Rate (HR);
-k_CO = CO(CO_idxs(1)) / T.CO(CO_idxs(1));
-k_PP = FEA(CO_idxs(1), 5) / (T.ABPSys(CO_idxs(1)) - T.ABPDias(CO_idxs(1)));
-k_MAP = FEA(CO_idxs(1), 6) / (T.ABPMean(CO_idxs(1)));
-k_HR = FEA(CO_idxs(1), 7) / T.HR(CO_idxs(1));
+
+[k_CO, k_PP, k_MAP, k_HR] = s00214.get_k(CO_idxs, CO, FEA, T);
+% k_CO = CO(CO_idxs(1)) / T.CO(CO_idxs(1));
+% k_PP = FEA(CO_idxs(1), 5) / (T.ABPSys(CO_idxs(1)) - T.ABPDias(CO_idxs(1)));
+% k_MAP = FEA(CO_idxs(1), 6) / (T.ABPMean(CO_idxs(1)));
+% k_HR = FEA(CO_idxs(1), 7) / T.HR(CO_idxs(1));
 
 % plot estimated cardiac output and measured CO
 subplot(4, 1, 1); hold on;
@@ -159,3 +162,64 @@ plot((1:43200)/3600, FEA(1:43200, 7), 'r');
 stem(T.ElapsedTime(CO_idxs) / 36e2, k_HR * T.HR(CO_idxs), 'b');
 ylabel("HR"); xlabel("Time (Hrs)");
 ylim padded; xlim tight;
+
+%% Q4
+figure;
+% plot estimated cardiac output and measured CO
+[k_CO, k_PP, k_MAP, k_HR] = s00214.get_k(CO_idxs, CO1, FEA1, T);
+subplot(4, 1, 1); hold on;
+plot(timehr_range, CO1(time_range) / k_CO, 'r');
+stem(T.ElapsedTime(CO_idxs)/36e2, T.CO(CO_idxs), 'b');
+ylabel("CO"); xlabel("Time (Hrs)")
+ylim padded; xlim tight;
+
+% plot estimated PP and measured PP
+subplot(4, 1, 2); hold on;
+plot(timehr_range, FEA(time_range, 5), 'r');
+stem(T.ElapsedTime(CO_idxs)/36e2, k_PP*(T.ABPSys(CO_idxs) - T.ABPDias(CO_idxs)), 'b');
+ylabel("PP"); xlabel("Time (Hrs)");
+ylim padded; xlim tight;
+
+% plot estimated MAP and measured MAP
+subplot(4, 1, 3); hold on;
+plot(timehr_range, FEA(time_range, 6), 'r');
+stem(T.ElapsedTime(CO_idxs)/36e2, k_MAP*T.ABPMean(CO_idxs), 'b');
+ylabel("MAP"); xlabel("Time (Hrs)");
+ylim padded; xlim tight;
+
+% plot estiamted HR and measured HR
+subplot(4, 1, 4); hold on;
+plot((1:43200)/3600, FEA(1:43200, 7), 'r');
+stem(T.ElapsedTime(CO_idxs) / 36e2, k_HR * T.HR(CO_idxs), 'b');
+ylabel("HR"); xlabel("Time (Hrs)");
+ylim padded; xlim tight;
+%%
+figure;
+[k_CO, k_PP, k_MAP, k_HR] = s00214.get_k(CO_idxs, CO2, FEA2, T);
+subplot(4, 1, 1); hold on;
+plot(timehr_range, CO1(time_range)/k_CO, 'r');
+stem(T.ElapsedTime(CO_idxs)/36e2, T.CO(CO_idxs), 'b');
+ylabel("CO"); xlabel("Time (Hrs)")
+ylim padded; xlim tight;
+
+% plot estimated PP and measured PP
+subplot(4, 1, 2); hold on;
+plot(timehr_range, FEA(time_range, 5), 'r');
+stem(T.ElapsedTime(CO_idxs)/36e2, k_PP*(T.ABPSys(CO_idxs) - T.ABPDias(CO_idxs)), 'b');
+ylabel("PP"); xlabel("Time (Hrs)");
+ylim padded; xlim tight;
+
+% plot estimated MAP and measured MAP
+subplot(4, 1, 3); hold on;
+plot(timehr_range, FEA(time_range, 6), 'r');
+stem(T.ElapsedTime(CO_idxs)/36e2, k_MAP*T.ABPMean(CO_idxs), 'b');
+ylabel("MAP"); xlabel("Time (Hrs)");
+ylim padded; xlim tight;
+
+% plot estiamted HR and measured HR
+subplot(4, 1, 4); hold on;
+plot((1:43200)/3600, FEA(1:43200, 7), 'r');
+stem(T.ElapsedTime(CO_idxs) / 36e2, k_HR * T.HR(CO_idxs), 'b');
+ylabel("HR"); xlabel("Time (Hrs)");
+ylim padded; xlim tight;
+
