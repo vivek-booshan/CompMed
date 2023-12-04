cc2febeef05a508fbe9a951335c9b921efa371b7
Vivek Booshan <69310398+vivek-booshan@users.noreply.github.com>
2023-12-04 18:19:03 -0500

    Update glm_part1.m

--- physiome/project2/glm_part1.m
+++ physiome/project2/glm_part1.m
@@ -1,22 +1,29 @@
 %import static training data (700 patients) - this is the basis of the simple model
 load('static_data_training.mat');
-varNames = {'Subject ID', 'Sepsis Status', 'Gender', 'Age', ...
-   'Respiratory Comorbidities', 'Heart Comorbidities', 'Infection'};
-static_train = array2table(static_train, 'VariableNames', varNames);
+% varNames = {'Subject ID', 'Sepsis Status', 'Gender', 'Age', ...
+%    'Respiratory Comorbidities', 'Heart Comorbidities', 'Infection'};
+% static_train = array2table(static_train, 'VariableNames', varNames);
 
 %The header variable contains the meaning of each column of static_train
 %generate simple glm
 %define Y = observations which should be loaded from clinical table
-y = static_train.("Sepsis Status");
-%Y = static_train(:,2);
+sepsis_status = find(strcmp(header, 'sepsis_status'));
+y = static_train(:, sepsis_status);
 
 %define X = covariate matrix by taking features from table. 
-%This currently only uses Gender as a covariate.
-X = table2array(static_train(:,3:7));
+keys = { ...
+    'respiratory_comorbidities', ...  
+    'cardiovascular_comorbidities', ...
+    'infection' ...
+    };
+static_idxs = zeros(1, length(keys));
+for i  = 1:length(keys)
+    static_idxs(i) = find(strcmp(header, keys(i)));
+end
+X = (static_train(:,static_idxs));
 
-%compute glm
 
-[B,dev,stats] = glmfit(X,y,'binomial', 'link', 'logit');
+[B,dev,stats] = glmfit(X,y,'binomial');
 %construct phat from parameters and X 
 Phat = 1 ./ (1 + exp( -[ones(size(X, 1), 1), X] * B)); 
 %Phat is the estimated probability of sepsis occurence for patients
@@ -27,10 +34,11 @@ LB = B - 1.96 * stats.se;
 Phat_UB = 1 ./ (1 + exp( -[ones(length(X), 1), X] * (UB)));
 Phat_LB = 1 ./ (1 + exp( -[ones(length(X), 1), X] * (LB)));
 
+
 % plot first 30 patients prediction, uncertainty and labels.
 clf;
 figure(1)
-plot(Phat(1:30))
+plot(Phat(1:30), 'r-')
 hold on
 plot(Phat_LB(1:30),'b-')
 hold on
@@ -38,10 +46,11 @@ plot(Phat_UB(1:30),'b-')
 hold on
 plot(y(1:30),'r*')
 title('Models for Each Patient')
+xlabel("Patient ID"); ylabel("P_hat");
 
 %test performance of models
-[threshold] = test_performance(Phat, Y);
-l=Y;
-[X,Y,T,AUC] = perfcurve(l,Phat, 1);
+[threshold] = test_performance(Phat, y);
+l=y;
+[X,y,T,AUC] = perfcurve(l,Phat, 1);
 figure;
-plot(X,Y)
+plot(X,y)
