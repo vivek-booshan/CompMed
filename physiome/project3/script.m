664bef6e81b0763609373b1b01e266a4f67411b1
Vivek Booshan <vivek.booshan@gmail.com>
2023-12-11 14:16:16 -0500

    add ode integrator and update script to plot

--- physiome/project3/script.m
+++ physiome/project3/script.m
@@ -1,27 +1,54 @@
-ax = 0.04;
+%%%%%% PARAMETERS %%%%%%
+ax = 0.04; 
 ag = 0.03;
+% Kx / Kg = 10
 Kx = [20 10];
 Kg = 0.1 * Kx;
-UI = 2; Ucho = 10 * UI
-figure;
-f = @(t, z) [
-    -ax * z(1) + ax*z(2);                   % eqn 17
-    -ax * z(2) + Kx(1) * ax * UI ;                % eqn 18 
-    -z(1) + z(4);                           % eqn 14
-    z(5);                                   % eqn 15
-    -2*ag*z(5) - ag^2 * z(4) + Kg(1) * ag^2 * Ucho % eqn 16
-    ];
-BG_u = ode45(f, 1:1:930, [0 0 90 0 0]);
-plot(BG_u.x, BG_u.y)
+% simulate up to 930 minutes
+tspan = 1:1:930;
 
-%%
-syms kg ag G s;
-F1 = (1/s) * (kg / (1 + s*(1/ag))^2) + (1/s)*G;
-f1 = ilaplace(F1);
+%%%% FUNCTIONS & DIFF SYSTEM %%%%%
+U1 = @(t) 2*(floor(t) == 180);    % branchless definition
+Ucho = @(t) 20*(floor(t) == 180); % branchless definition
+f = @(t,z,i) [
+    -ax*z(1) + ax*z(2);                            %eqn 17 (dX)
+    -ax*z(2) + Kx(i)*ax*U1(t);                     %eqn 18 (dX1)
+    -z(1) + z(4);                                  %eqn 14 (dG)
+     z(5);                                         %eqn 15 (dUg)
+    -2*ag*z(5) - ag^2*z(4) + Kg(i)*ag^2 * Ucho(t); %eqn 16 (ddUg)
+];
 
-t = 1:1:930;
-G = 90;
-kg = 2;
-ag = 0.03;
-f1 = G + kg + kg*exp(-ag*t) - ag*kg*t.*exp(-ag*t);
-plot(t, f1);
+subplot(2, 1, 1);
+hold on;
+for i = 1:2
+    BG_u = ode4(f, tspan, [0 0 90 0 0], i); % credit: Luis Sanchez
+    plot(tspan, BG_u(:, 3));
+end
+title('Bock et al Fig 4')
+subtitle("[BG] with meal and bolus")
+xlabel('Time (min)')
+axis([0 930 45 100])
+ylabel('BG concentration (mg/dl)')
+grid;
+legend('Kx = 20','Kx = 10','Location','SouthEast')
+
+%%%% ASSUME ONLY INSULIN BOLUS (UCHO = 0) %%%%
+Ucho = @(t) 0;
+f = @(t,z,i) [
+    -ax*z(1) + ax*z(2);                            %eqn 17 (dX)
+    -ax*z(2) + Kx(i)*ax*U1(t);                     %eqn 18 (dX1)
+    -z(1) + z(4);                                  %eqn 14 (dG)
+     z(5);                                         %eqn 15 (dUg)
+    -2*ag*z(5) - ag^2*z(4) + Kg(i)*ag^2 * Ucho(t); %eqn 16 (ddUg)
+];
+subplot(2, 1, 2);
+hold on 
+for i = 1:2
+    BG_u = ode4(f, tspan, [0 0 90 0 0], i); % credit: Luis Sanchez
+    plot(tspan, BG_u(:, 3));
+end
+subtitle("[BG] with only insulin bolus");
+xlabel('Time (hours)');
+ylabel('BG concentration (mg/dl)');
+grid; axis([0 930 45 90]);
+legend('Kx = 20','Kx = 10','Location','NorthEast');
